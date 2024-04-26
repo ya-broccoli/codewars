ab201f6b5c82d329838e0bd433101b6e08de39c2
Raevskaya Natalie <ya.brokkoli@gmail.com>
2024-04-26 12:51:48 +0300

    function to calculate Average number of Array

---
+++ 7kyu/calculate-average-number-of-array.js
@@ -0,0 +1,11 @@
+// ❓ DESCRIPTION:
+// Given an array of integers, calculate the Average of these numbers.
+
+// Main challenge is to write shortest and compact function for it.
+
+// For example: var a = [0, 1, 2];
+// avg(a) // output should be 1
+// Output of function will be also checked in tests, however most important is to write the shortest possible function (code length < 100). Input will always be valid.
+
+// ✅ SOLUTION: 
+const avg = (a)  => a.reduce((acc, el) => acc += el, 0) / a.length;
