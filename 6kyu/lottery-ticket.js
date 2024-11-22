ac883e3c1547b4629feb4feb91d7e420ad57099e
Raevskaya Natalie <ya.brokkoli@gmail.com>
2024-11-22 15:49:27 +0300

    lottery ticket

---
+++ 6kyu/lottery-ticket.js
@@ -0,0 +1,24 @@
+// ❓ DESCRIPTION:
+// Time to win the lottery!
+
+// Given a lottery ticket (ticket), represented by an array of 2-value arrays, you must find out if you've won the jackpot.
+
+// Example ticket:
+
+// [ [ 'ABC', 65 ], [ 'HGR', 74 ], [ 'BYHT', 74 ] ]
+// To do this, you must first count the 'mini-wins' on your ticket. Each subarray has both a string and a number within it. If the character code of any of the characters in the string matches the number, you get a mini win. Note you can only have one mini win per sub array.
+
+// Once you have counted all of your mini wins, compare that number to the other input provided (win). If your total is more than or equal to (win), return 'Winner!'. Else return 'Loser!'.
+
+// All inputs will be in the correct format. Strings on tickets are not always the same length.
+
+// ✅ SOLUTION:
+function bingo(ticket, win) {
+  let count = 0;
+  for (let i = 0; i < ticket.length; i++) {
+    if (ticket[i][0].split("").some((el) => el.charCodeAt() === ticket[i][1])) {
+      count++;
+    }
+  }
+  return count >= win ? "Winner!" : "Loser!";
+}
